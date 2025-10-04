6c492b4b1a7f15ff1d06bd60f9e4fa4344962391
Luca <luca.antonelli@gmx.ch>
2025-10-04 21:23:51 +0200

    added nvim-dap and nvim-dap-ui for tauri development

---
+++ lua/plugins/nvim-dap.lua
@@ -0,0 +1,48 @@
+return {
+	"mfussenegger/nvim-dap",
+	config = function()
+		local dap = require("dap")
+
+		dap.adapters.codelldb = {
+			type = "server",
+			port = "${port}",
+			executable = {
+				-- Change this to your path!
+				command = "/opt/codelldb/adapter/codelldb",
+				args = { "--port", "${port}" },
+			},
+		}
+
+		dap.configurations.rust = {
+			{
+				name = "Launch file",
+				type = "codelldb",
+				request = "launch",
+				program = function()
+					return vim.fn.input("Path to executable: ", vim.fn.getcwd() .. "/target/debug/", "file")
+				end,
+				cwd = "${workspaceFolder}",
+				stopOnEntry = false,
+			},
+		}
+		local dapui = require("dapui")
+
+		dapui.setup()
+
+		dap.listeners.before.attach.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.launch.dapui_config = function()
+			dapui.open()
+		end
+		dap.listeners.before.event_terminated.dapui_config = function()
+			dapui.close()
+		end
+		dap.listeners.before.event_exited.dapui_config = function()
+			dapui.close()
+		end
+
+		vim.fn.sign_define("DapBreakpoint", { text = "🟥", texthl = "", linehl = "", numhl = "" })
+		vim.fn.sign_define("DapStopped", { text = "▶️", texthl = "", linehl = "", numhl = "" })
+	end,
+}
